fe23328e94a99a5fe05dc5fe0b36d8b3ae332733
minhlh <hongminhle290@gmail.com>
2024-03-16 19:44:33 +0700

    init

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="navbar navbar-sm navbar-footer border-top">
+    <div class="container-fluid">
+        <span>&copy; 2023 <a href="https://themeforest.net/item/limitless-responsive-web-application-kit/13080328">LHM DEV</a></span>
+
+    </div>
+</div>
---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,24 @@
+{{--<!-- Core JS files -->--}}
+<script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+{{--<!-- /core JS files -->--}}
+
+{{--<!-- Theme JS files -->--}}
+<script src="{{ asset('assets/js/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/noty/noty.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/notifications/sweet_alert.min.js') }}"></script>
+{{--<script src="{{ asset('vendor/laravel-filemanager/js/stand-alone-button.js') }}"></script>--}}
+<script src="{{ asset('assets/js/money/simple.money.format.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/ui/moment/moment.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/pickers/daterangepicker.js') }}"></script>
+<script src="{{ asset('assets/js/app.js') }}"></script>
+
+{{--<!-- /theme JS files -->--}}
+
+
+@vite('resources/js/app.js')
+
+{{--<!-- JS custom -->--}}
+
+
+@yield('script_custom')
+{{--<!-- /JS custom  -->--}}
