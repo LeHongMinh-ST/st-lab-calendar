6c95682d372f880aa2fa528813fdc1b244c309e7
minhlh <hongminhle290@gmail.com>
2024-03-17 20:40:16 +0700

    init

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,16 @@
 <x-main-layout>
-    Hello ST
+    <div class="content">
+
+        <!-- Basic view -->
+        <div class="card">
+            <div class="card-header">
+                <h5 class="mb-0">ST Lab Calendar</h5>
+            </div>
+
+            <div class="card-body">
+                <div class="fullcalendar-basic"></div>
+            </div>
+        </div>
+        <!-- /basic view -->
+    </div>
 </x-main-layout>
