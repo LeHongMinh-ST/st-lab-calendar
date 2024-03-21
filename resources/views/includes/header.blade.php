acf0829035cd0710cc293fbda6c7855049fd9afe
minhlh <hongminhle290@gmail.com>
2024-03-22 00:21:36 +0700

    add sidebar

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,7 +7,7 @@
         </div>
 
         <div class="navbar-brand flex-1 flex-lg-0">
-            <a href="index.html" class="d-inline-flex align-items-center">
+            <a href="{{route('home')}}" class="d-inline-flex align-items-center">
                 <img src="https://demo.interface.club/limitless/demo/template/assets/images/logo_icon.svg" alt="">
                 <img src="https://demo.interface.club/limitless/demo/template/assets/images/logo_text_light.svg" class="d-none d-sm-inline-block h-16px ms-3" alt="">
             </a>
--- resources/views/pages/teams.blade.php
+++ resources/views/pages/teams.blade.php
@@ -1,29 +1,22 @@
 <x-main-layout>
-    <div class="content teams">
-        <div class="card">
-            <div class="card-header">
-                <h4>Danh sách nhóm</h4>
-            </div>
-            <div class="card-body">
-                <div class="list">
-                    <x-team-item
-                        :thumbnail="asset('assets/images/logoST.jpg')"
-                        title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
-                    />
-                    <x-team-item
-                        :thumbnail="asset('assets/images/logoST.jpg')"
-                        title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
-                    />
-                    <x-team-item
-                        :thumbnail="asset('assets/images/logoST.jpg')"
-                        title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
-                    />
-                    <x-team-item
-                        :thumbnail="asset('assets/images/logoST.jpg')"
-                        title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
-                    />
-                </div>
-            </div>
+    <div class="content teams d-flex justify-content-center align-items-center">
+        <div class="list">
+            <x-team-item
+                :thumbnail="asset('assets/images/logoST.jpg')"
+                title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
+            />
+            <x-team-item
+                :thumbnail="asset('assets/images/logoST.jpg')"
+                title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
+            />
+            <x-team-item
+                :thumbnail="asset('assets/images/logoST.jpg')"
+                title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
+            />
+            <x-team-item
+                :thumbnail="asset('assets/images/logoST.jpg')"
+                title="ST-TEAM - NHÓM NGHIÊN CỨU VÀ PHÁT TRIỂN PHẦN MỀM - BỘ MÔN CÔNG NGHỆ PHẦN MỀM"
+            />
         </div>
     </div>
 </x-main-layout>
