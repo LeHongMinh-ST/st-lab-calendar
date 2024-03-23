acc857f187cea3ebed12fec41efc3ec298d9ac14
minhlh <hongminhle290@gmail.com>
2024-03-23 15:45:15 +0700

    add calendar id

--- database/migrations/2024_03_23_084334_add_calendar_id_column_events_table.php
+++ database/migrations/2024_03_23_084334_add_calendar_id_column_events_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('teams', function (Blueprint $table) {
-            if (!Schema::hasColumn('teams', 'slug')) {
-                $table->string('slug')->unique()->after('description');
+        Schema::table('events', function (Blueprint $table) {
+            if (!Schema::hasColumn('events', 'calendar_id')) {
+                $table->bigInteger('calendar_id')->nullable()->index()->after('id');
             }
         });
     }
@@ -22,8 +23,8 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::table('teams', function (Blueprint $table) {
-
+        Schema::table('events', function (Blueprint $table) {
+            //
         });
     }
 };
