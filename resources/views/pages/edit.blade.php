57be2f52f9666e9fbbdfe567ab075fcc75355212
Trương Tuấn Hoàng <6662012@sv.vnua.edu.vn>
2024-03-26 22:25:55 +0700

    giao-dien-edit-users

---
+++ resources/views/pages/edit.blade.php
@@ -0,0 +1,151 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <div class="page-header page-header-light shadow">
+            <div class="page-header-content d-lg-flex">
+                <div class="d-flex">
+                    <h4 class="page-title mb-0">
+                        Home - <span class="fw-normal">Edit</span>
+                    </h4>
+
+                    <a href="#page_header" class="btn btn-light align-self-center collapsed d-lg-none border-transparent rounded-pill p-0 ms-auto" data-bs-toggle="collapse">
+                        <i class="ph-caret-down collapsible-indicator ph-sm m-1"></i>
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="page-header-content d-lg-flex border-top">
+                <div class="d-flex">
+                    <div class="breadcrumb py-2">
+                        <a href="index.html" class="breadcrumb-item"><i class="ph-house"></i></a>
+                        <a href="#" class="breadcrumb-item">Home</a>
+                        <span class="breadcrumb-item active">Edit</span>
+                    </div>
+
+                    <a href="#breadcrumb_elements" class="btn btn-light align-self-center collapsed d-lg-none border-transparent rounded-pill p-0 ms-auto" data-bs-toggle="collapse">
+                        <i class="ph-caret-down collapsible-indicator ph-sm m-1"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </x-slot>
+
+
+    <div class="content">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">User profile</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Avatar</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact-tab-pane" type="button" role="tab" aria-controls="contact-tab-pane" aria-selected="false">Change password</button>
+            </li>
+        </ul>
+        <div class="tab-content" id="myTabContent" style="background-color: white;">
+            <div class="tab-pane fade show active" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+            <form style="padding: 20px;">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="mb-3">
+                            <label class="form-label">First Name:</label>
+                            <input type="text" name="firstname" class="form-control" value="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="mb-3">
+                            <label class="form-label">Last name:</label>
+                            <input type="text" name="lastname" class="form-control" value="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="mb-3">
+                            <label class="form-label">Username:</label>
+                            <input type="text" name="username" class="form-control" value="">
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                    <div class="mb-3">
+                            <label class="form-label">Email:</label>
+                            <input type="email" name="email" class="form-control" value="">
+                        </div>
+                    </div>
+                </div>
+                    
+                    <hr>
+                    <div class="d-flex justify-content-end align-items-center">
+                        <button type="submit" class="btn btn-primary">Update</button>
+                    </div>
+            </form>
+
+
+            </div>
+            <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+            <div class="mb-3" style="padding: 20px;">
+            
+            <form action="">
+                <label class="form-label">Chọn ảnh đại diện:</label>
+                <input type="file" class="form-control">
+                <div class="form-text text-muted">Tên tệp khả dụng: gif, png, jpg.</div>
+                <hr>
+                <div class="d-flex justify-content-end align-items-center">
+                    <button type="submit" class="btn btn-primary">Update</button>
+                </div>
+            </form>
+
+            </div>
+            </div>
+            <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">
+            <div class="row" style="padding: 20px;">
+            <div class="card-body">
+                <form action="#">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Username</label>
+                                <input type="text" value="" readonly class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Mật khẩu hiện tại</label>
+                                <input type="password" value="" readonly class="form-control">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Mật khẩu mới</label>
+                                <input type="password" placeholder="Enter new password" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <div class="mb-3">
+                                <label class="form-label">Nhập lại mật khẩu mới</label>
+                                <input type="password" placeholder="Repeat new password" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <hr>
+                    <div class="text-end">
+                        <button type="submit" class="btn btn-primary">Save changes</button>
+                    </div>
+                </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</x-admin-layout>
