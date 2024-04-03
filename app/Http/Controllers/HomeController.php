703a98362752b92204a6674cc6a62935afa883bd
minhlh-hapo <minhlh@haposoft.com>
2024-04-03 13:52:48 +0700

    fix timestamp

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Enums\ActivityType;
+use App\Models\Event;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Application;
@@ -11,11 +13,19 @@ use Illuminate\Support\Carbon;
 
 class HomeController extends Controller
 {
+    private const LIMIT_WEEKS = 2;
     public function activities(): View|Application|Factory
     {
-        $now = Carbon::now();
+        $now = Carbon::now()->startOfDay()->timestamp;
+        $endDate = Carbon::now()->addWeeks(self::LIMIT_WEEKS)->timestamp;
 
-        $endDate = Carbon::now()->addWeeks(2);
+        $seminars = Event::where('day', '>=', $now)
+            ->where('day', '<=', $endDate)
+            ->whereHas('activity', function ($query): void {
+                $query->where('type', ActivityType::Seminar);
+            })
+            ->with(['user', 'team', 'activity'])
+            ->get();
 
         return view('activities', compact('seminars'));
     }
